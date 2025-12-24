72e58c28c836965d59607d498a58603c823bde3f
Divyam Kumar Pandey <divyamkumarp@gmail.com>
2025-12-24 05:51:21 +0530

    Add DecompressFile function to utils/compressor.go for handling gzip file decompression

--- pkg/utils/compressor.go
+++ pkg/utils/compressor.go
@@ -2,8 +2,11 @@ package utils
 
 import (
 	"compress/gzip"
+	"fmt"
 	"io"
 	"os"
+	"path/filepath"
+	"strings"
 )
 
 
@@ -25,6 +28,37 @@ func CompressFile(inputPath string) (string, error) {
 	gzWriter.Close()
 
 	if _, err := io.Copy(gzWriter, in); err != nil {
+		return "", err
+	}
+
+	return outputPath, nil
+}
+
+func DecompressFile(gzipPath string) (string, error) {
+	if filepath.Ext(gzipPath) != ".gz" {
+		return "", fmt.Errorf("not a gzip file")
+	}
+
+	in, err := os.Open(gzipPath)
+	if err != nil {
+		return "", err
+	}
+	defer in.Close()
+
+	gzReader, err := gzip.NewReader(in)
+	if err != nil {
+		return "", err
+	}
+	defer gzReader.Close()
+
+	outputPath := strings.TrimSuffix(gzipPath, ".gz")
+	out, err := os.Create(outputPath)
+	if err != nil {
+		return "", err
+	}
+	defer out.Close()
+
+    if _, err := io.Copy(out, gzReader); err != nil {
 		return "", err
 	}
 
